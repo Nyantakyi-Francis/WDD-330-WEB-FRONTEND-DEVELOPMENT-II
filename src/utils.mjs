5ce5f382e39cab9792945cfcc0f15993a7eb25ac
Nyantakyi-Francis <nyantakyifrancis54@gmail.com@byupathway.edu>
2025-09-06 18:28:24 +0000

    Completed W02 Individual Activity

---
+++ src/utils.mjs
@@ -0,0 +1,31 @@
+import { renderListWithTemplate } from './utils.mjs';
+
+function productCardTemplate(product) {
+    return `
+    <li class="product-card">
+      <a href="product_pages/index.html?product=${product.id}">
+        <img src="${product.image}" alt="${product.name} image">
+        <h3 class="card__brand">${product.brand.name}</h3>
+        <h2 class="card__name">${product.name}</h2>
+        <p class="product-card__price">$${product.finalPrice}</p>
+      </a>
+    </li>
+  `;
+}
+
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+        this.renderList(list);
+    }
+
+    renderList(list) {
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
+    }
+}
